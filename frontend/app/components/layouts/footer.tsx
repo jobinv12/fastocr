c57cc871dce154c73bb55c17f5aeecf06ab936a0
Jobin Thavu Varghese <jobintv@gmail.com>
2025-09-08 10:59:24 +0530

    changed footer

--- frontend/app/components/layouts/footer.tsx
+++ frontend/app/components/layouts/footer.tsx
@@ -1,11 +1,13 @@
-import { FaGithub } from "react-icons/fa6";
+import { CodeXmlIcon } from "lucide-react";
 import { Link } from "react-router";
 
 export function Footer() {
   return (
     <footer className="fixed flex flex-row inset-x-0 bottom-0 p-4 text-center justify-center gap-2">
       <Link to={"/"}>
-        <p className="hover:underline">Contribute on Github</p>
+        <p className="flex flex-row gap-2">
+          Made with by <CodeXmlIcon /> by Human.
+        </p>
       </Link>
     </footer>
   );
