dd1eb93f4f11ffa5fddd0a15852efd9d07da7c8e
Jobin Thavu Varghese <jobintv@gmail.com>
2025-09-08 11:01:52 +0530

    removed link from footer

--- frontend/app/components/layouts/footer.tsx
+++ frontend/app/components/layouts/footer.tsx
@@ -1,14 +1,11 @@
 import { CodeXmlIcon } from "lucide-react";
-import { Link } from "react-router";
 
 export function Footer() {
   return (
     <footer className="fixed flex flex-row inset-x-0 bottom-0 p-4 text-center justify-center gap-2">
-      <Link to={"/"}>
-        <p className="flex flex-row gap-2">
-          Made with by <CodeXmlIcon /> by Human.
-        </p>
-      </Link>
+      <p className="flex flex-row gap-2">
+        Made with by <CodeXmlIcon /> by Human.
+      </p>
     </footer>
   );
 }
