319709c2dbee42464c790d0eb2c7296164aa61f8
Jobin Thavu Varghese <jobintv@gmail.com>
2025-09-20 21:07:26 +0530

    added glass effect

--- frontend/app/components/layouts/navbar.tsx
+++ frontend/app/components/layouts/navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <nav className="fixed w-full top-0 left-0 py-1 px-4">
+    <nav className="fixed w-full top-0 left-0 mt-2 py-1 px-4 bg-white/40 backdrop-blur-[5px] border rounded-full border-neutral-400/20">
       <div className="flex flex-row text-5xl font-bold font-drifline">
         <p className="text-blue-950">Fast</p>
         <p className="text-orange-600">OCR</p>
